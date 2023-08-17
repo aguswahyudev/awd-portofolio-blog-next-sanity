d22e438d6c14aea75d517cdcdb568229ac1f2780
I Putu Agus Wahyu Dupayana <75474419+putuwahyu29@users.noreply.github.com>
2023-08-17 19:54:45 +0800

    Order Fix

--- src/app/[lang]/(user)/page.tsx
+++ src/app/[lang]/(user)/page.tsx
@@ -33,7 +33,7 @@ export default async function HomePage({ params: { lang } }: Props) {
     linkPreview,
     linkSource,
     technologies[]->
-  } | order(_createdAt desc)`,
+  } | order(_updatedAt desc)`,
     { lang }
   );
 
